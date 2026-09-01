39bbc7d2a05edb7a5786d9fe26cea209d8f016bf
aniketgajjar <asalparaaniket@gmail.com>
2026-09-01 10:58:58 +0530

    feat: add URL model and schema

--- src/models/url.model.js
+++ src/models/url.model.js
@@ -1,32 +1,34 @@
 const mongoose = require('mongoose');
 
-const clickSchema = new mongoose.Schema ({
-    ip : {
-        type: String, 
-        default: 'Unknown'
-    }, 
-    userAgent : {
-        type: String, 
-        default: 'Unknown'
-    }, 
-    referrer : {
-        type : String, 
-        default: 'Direct'
-    },
-}, 
-    { timestamps : true }
-);
+// const clickSchema = new mongoose.Schema ({
+//     ip : {
+//         type: String, 
+//         default: 'Unknown'
+//     }, 
+//     userAgent : {
+//         type: String, 
+//         default: 'Unknown'
+//     }, 
+//     referrer : {
+//         type : String, 
+//         default: 'Direct'
+//     },
+// }, 
+//     { timestamps : true }
+// );
 
 
 const urlSchema = new mongoose.Schema = ({
     originalUrl: { 
         type: String, 
-        required: true 
+        required: true, 
+        trim : true
     },
     shortCode: { 
         type: String, 
         required: true, 
-        unique: true 
+        unique: true, 
+        trim : true
     },
     clicks: [clickSchema], 
     totalClicks: { 
